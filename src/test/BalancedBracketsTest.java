e848186d14ffbe53ac5fe4e8128d55c2370906d8
MulesKC <rmmcp3@gmail.com>
2020-01-23 22:39:59 -0600

    complete

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,87 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void singleCorrectBrackets() {
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Chiefs]"));
+
+    }
+
+    @Test
+    public void missingCloseBracket() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Chiefs"));
+
+    }
+
+    @Test
+    public void missingOpenBracket() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Chiefs]"));
+
+    }
+
+    @Test
+    public void noBrackets() {
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Chiefs"));
+
+    }
+
+    @Test
+    public void bracketsWrongOrderBeforeWord() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][Chiefs"));
+
+    }
+
+    @Test
+    public void bracketsWrongOrderAfterWord() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Chiefs]["));
+
+    }
+
+    @Test
+    public void emptyString() {
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+
+    }
+
+    @Test
+    public void twoCorrectBracketSets() {
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Kansas] [City]"));
+
+    }
+
+    @Test
+    public void threeCorrectNestedBrackets() {
+
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Kansas[City[Chiefs]]]"));
+
+    }
+
+    @Test
+    public void nestedMissingCloseBracket() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Hello[World[]]"));
+
+    }
+
+    @Test
+    public void nestedMissingOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Hello[World]]]"));
+    }
+
+    @Test
+    public void twoSetsWithinBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[Kansas] [City]]"));
+    }
+
 
 
 }
